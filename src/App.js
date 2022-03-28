3e96e7b6fa25390f7d13715d7f2e0f13729abb8d
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-03-28 10:53:01 +0100

    import components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Dashboard from "./Components/Dashboard";
+import Reset from "./Components/Reset";
+import Home from "./Components/Home";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={Home }/>
+        <Route path="/dashboard" element={Dashboard}/>
+        <Route path="/reset" element={Reset }/>
+      </Routes>
+      </BrowserRouter>
     
     </div>
   );
